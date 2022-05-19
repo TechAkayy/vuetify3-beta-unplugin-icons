0d204ff6e12339097586c576fef7741234131c35
akkayy <ahmed.kaja@outlook.com>
2022-05-19 22:45:42 +1000

    Installed and configured unplugins, installed iconify json for unicons

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,32 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from '@vuetify/vite-plugin'
+import Components from 'unplugin-vue-components/vite'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
 
 const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
-    vuetify({
-      autoImport: true,
-    }),
-  ],
-  define: { 'process.env': {} },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-  /* remove the need to specify .vue files https://vitejs.dev/config/#resolve-extensions
+	plugins: [
+		vue(),
+		// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+		vuetify({
+			autoImport: true
+		}),
+		Components({
+			resolvers: [IconsResolver()]
+		}),
+		Icons({ compiler: 'vue3' })
+	],
+	define: { 'process.env': {} },
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src')
+		}
+	}
+	/* remove the need to specify .vue files https://vitejs.dev/config/#resolve-extensions
   resolve: {
     extensions: [
       '.js',
