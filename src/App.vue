fb870a543264104cb1d30fe70eef4e85adafd72a
akkayy <ahmed.kaja@outlook.com>
2022-05-20 20:42:42 +1000

    Using slots

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,51 @@
 	<v-app>
 		<v-main>
 			<i-uil-android />
-			<v-alert :icon="uilShoppingCart"
+			<!-- <v-alert :icon="uilShoppingCart"
 				>VAlert - icon is passed as a prop</v-alert
-			>
+			> -->
+			<br />
+			<v-alert>
+				<template #default> VAlert - icon is passed on #append1 slot </template>
+				<template #append>
+					<i-uil-shopping-cart />
+				</template>
+			</v-alert>
+			<br />
+			<v-alert>
+				<template #default> VAlert - icon is passed on #append2 slot </template>
+				<template #append>
+					<i
+						class="v-icon notranslate v-icon--size-default v-theme--light"
+						aria-hidden="true"
+					>
+						<i-uil-shopping-cart />
+					</i>
+				</template>
+			</v-alert>
+			<br />
+			<v-alert>
+				<template #default> VAlert - icon is passed #prepend1 slot </template>
+				<template #prepend>
+					<i-uil-shopping-cart />
+				</template>
+			</v-alert>
+			<br />
+			<v-alert>
+				<template #default> VAlert - icon is passed #prepend2 slot </template>
+				<template #prepend>
+					<i
+						class="v-icon notranslate v-icon--size-default v-theme--light"
+						aria-hidden="true"
+					>
+						<i-uil-shopping-cart />
+					</i>
+				</template>
+			</v-alert>
 		</v-main>
 	</v-app>
 </template>
 
 <script setup>
-	import uilShoppingCart from '~icons/uil/shoppingCart'
+	// import uilShoppingCart from '~icons/uil/shoppingCart';
 </script>
