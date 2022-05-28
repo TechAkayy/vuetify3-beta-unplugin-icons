bc1179a86a1bec15410bb3c60f4e61bf7692582a
akkayy <techakayy@gmail.com>
2022-05-28 20:31:59 +1000

    Testing on other components, nightly next build used

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,24 @@
 <template>
 	<v-app>
+		<v-app-bar
+			:collapse="!collapseOnScroll"
+			:collapse-on-scroll="collapseOnScroll"
+			absolute
+			color="deep-purple accent-4"
+			dark
+			scroll-target="#scrolling-techniques-6"
+		>
+			<v-app-bar-nav-icon :icon="uilShoppingCart"></v-app-bar-nav-icon>
+			<v-avatar color="indigo" :icon="uilShoppingCart"> </v-avatar>
+			<v-badge
+				:content="messages"
+				:value="messages"
+				color="green"
+				overlap
+				:icon="uilShoppingCart"
+			>
+			</v-badge>
+		</v-app-bar>
 		<v-main>
 			<v-alert :icon="uilShoppingCart"
 				>VAlert - icon is passed as a prop</v-alert
