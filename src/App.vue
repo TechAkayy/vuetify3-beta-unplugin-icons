33130b43b07ac9cc43c1619cc7e9f6b1d6e52dfe
akkayy <techakayy@gmail.com>
2022-05-28 20:14:31 +1000

    VAlert defn issue, VIcon seems to be okay

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,17 @@
 <template>
 	<v-app>
 		<v-main>
-			<i-uil-android />
-			<!-- <v-alert :icon="uilShoppingCart"
+			<v-alert :icon="uilShoppingCart"
 				>VAlert - icon is passed as a prop</v-alert
-			> -->
-			<br />
-			<v-alert>
-				<template #default> VAlert - icon is passed on #append1 slot </template>
-				<template #append>
-					<i-uil-shopping-cart />
-				</template>
-			</v-alert>
-			<br />
-			<v-alert>
-				<template #default> VAlert - icon is passed on #append2 slot </template>
-				<template #append>
-					<i
-						class="v-icon notranslate v-icon--size-default v-theme--light"
-						aria-hidden="true"
-					>
-						<i-uil-shopping-cart />
-					</i>
-				</template>
-			</v-alert>
-			<br />
-			<v-alert>
-				<template #default> VAlert - icon is passed #prepend1 slot </template>
-				<template #prepend>
-					<i-uil-shopping-cart />
-				</template>
-			</v-alert>
-			<br />
-			<v-alert>
-				<template #default> VAlert - icon is passed #prepend2 slot </template>
-				<template #prepend>
-					<i
-						class="v-icon notranslate v-icon--size-default v-theme--light"
-						aria-hidden="true"
-					>
-						<i-uil-shopping-cart />
-					</i>
-				</template>
-			</v-alert>
+			>
+			<div>
+				<v-icon :icon="uilShoppingCart"></v-icon
+				><span> VIcon - icon is passed as a prop</span>
+			</div>
 		</v-main>
 	</v-app>
 </template>
 
 <script setup>
-	// import uilShoppingCart from '~icons/uil/shoppingCart';
+	import uilShoppingCart from '~icons/uil/shoppingCart'
 </script>
